3f589a6d32f3266a566b122d372102bb2d453622
magdalenakosinska <59508924+magdalenakosinska@users.noreply.github.com>
2023-01-01 18:43:34 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -76,10 +76,11 @@ function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
 
-  forecast.forEach(function (forecastDay) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
       <div class="col-2">
         <div class="weather-forecast-date">
           ${forecastDay.time}
@@ -97,6 +98,7 @@ function displayForecast(response) {
         </div>
       </div>
   `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
